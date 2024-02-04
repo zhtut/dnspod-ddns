b048c8553479d13c52810d67f3128294a0af0034
zhtut <>
2024-02-04 23:19:46 +0800

    update files

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-app_name="dnspod-ddns"
+app_name="dnspod_ddns"
 docker build --platform linux/amd64 -t shutut/$app_name:amd64 --push .
 docker build --platform linux/arm64/v8 -t shutut/$app_name:arm64 --push .
