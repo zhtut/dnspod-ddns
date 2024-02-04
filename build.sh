dc4c2e590724447b9b779a409b574c00178ea8a6
zhtut <>
2024-02-05 03:03:10 +0800

    update build

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 app_name="dnspod_ddns"
 arch=$(uname -m)
+if [ $arch == 'x86_64' ]; then
+  arch="amd64"
+fi
 name=shutut
-docker build -t $name/$app_name:$arch --push .
+docker buildx build -t $name/$app_name:$arch --push .
 # swift还不支持跨平台构建，在mac m1上运行amd64的镜像，swift的命令都报错了，
 # 所以这里需要分开构建，然后再通过manifest合到一起
 docker manifest create \
